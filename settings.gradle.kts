626bea80e3368366599833cd615961b3ae0d4ea3
AdonisAlcantara <dondon032002@gmail.com>
2023-12-08 22:02:09 +0800

    1st

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "AdonisAlcantara-Block2-Quiz"
+include(":app")
+ 

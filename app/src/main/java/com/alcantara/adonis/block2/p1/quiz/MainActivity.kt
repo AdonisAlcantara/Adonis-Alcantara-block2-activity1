448781171e5077ad35d332c3d763d34abaae5c76
AdonisAlcantara <dondon032002@gmail.com>
2023-12-08 22:13:57 +0800

    5th

--- app/src/main/java/com/alcantara/adonis/block2/p1/quiz/MainActivity.kt
+++ app/src/main/java/com/alcantara/adonis/block2/p1/quiz/MainActivity.kt
@@ -17,6 +17,11 @@ import java.util.Locale
 
 class MainActivity : ComponentActivity() {
     private lateinit var nextButton: Button
+    private var userSelectedDate: Long = 0
+    private val maxBookingsPerTimeSlot = 1  // Limit one booking per time slot
+    private val maxBookingsPerDate = 5  // Limit 5 bookings per day
+    private val bookedTimeSlots = mutableMapOf<String, Int>()
+    private val bookedDates = mutableMapOf<String, Int>()
 
     private val timeZone = java.util.TimeZone.getTimeZone("Asia/Manila")
     private val format = SimpleDateFormat("yyyy-MM-dd h:mm a", Locale.US)
